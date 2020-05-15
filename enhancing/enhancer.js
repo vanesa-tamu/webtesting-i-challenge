67b7acceae0467a15df5e1b2af91d00f8eacbc1d
vanesa-tamu <vahe5495@tamu.edu>
2020-05-14 19:41:08 -0500

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,19 +2,48 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  if(item.enhancement < 20){
+    item = {
+      ...item,
+      enhancement: item.enhancement + 1
+    }
+  }
+  return item;
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15){
+    item = {
+      ...item,
+      durability: item.durability - 5
+    }
+  }
+  if(item.enhancement >= 15){
+    let durability;
+    durability = item.durability - 10;
+    item = {
+      ...item,
+      durability: durability
+    }
+  }
+  if(item.enhancement > 16){
+    item = {
+      ...item,
+      enhancement: item.enhancement - 1
+    }
+  }
+  return item;
 }
 
 function repair(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability: 100
+  }
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,76 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('Enhancer tests', () => {
+    it('Succeed()', () => {
+        // Test with enhancement level 20 - expect no changes
+        expect(enhancer.succeed({
+            name: 'Shovel',
+            durability: 50,
+            enhancement: 20
+        })).toEqual({
+            name: 'Shovel',
+            durability: 50,
+            enhancement: 20
+        });
+        
+        // Test with enchancment level 15 - expect level up +1
+        expect(enhancer.succeed({
+            name: 'Axe',
+            durability: 20,
+            enhancement: 15
+        })).toEqual({
+            name: 'Axe',
+            durability: 20,
+            enhancement: 16
+        });
+    })
+
+    it('Fail()', () => {
+        // Test when enhancement is less than 15 - expect durability -5
+        expect(enhancer.fail({
+            name: 'Shovel',
+            durability: 50,
+            enhancement: 14
+        })).toEqual({
+            name: 'Shovel',
+            durability: 45,
+            enhancement: 14
+        });
+
+        // Test when enhancement is 15 - expect durability -10
+        expect(enhancer.fail({
+            name: 'Axe',
+            durability: 80,
+            enhancement: 15
+        })).toEqual({
+            name: 'Axe',
+            durability: 70,
+            enhancement: 15
+        });
+
+        // Test when enhancement is 17 - expect enhancement to -1
+        expect(enhancer.fail({
+            name: 'Sword',
+            durability: 50,
+            enhancement: 17
+        })).toEqual({
+            name: 'Sword',
+            durability: 40,
+            enhancement: 16
+        });
+    })
+
+    it('Repair()', () => {
+        // Test that durability goes to 100
+        expect(enhancer.repair({
+            name: 'Shovel',
+            durability: 20,
+            enhancement: 20
+        })).toEqual({
+            name: 'Shovel',
+            durability: 100,
+            enhancement: 20
+        })
+    })
+})
